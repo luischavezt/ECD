ad4d8c5a66666301f94cef699a9d790a07345c1d
luischavezt <luischavezt10@gmail.com>
2026-06-06 16:14:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -257,4 +257,12 @@ function actualizarBarraProgreso() {
     barra.innerText = porcentaje + "%";
 }
 
+window.addEventListener('load', () => {
+    const splash = document.getElementById('splash-screen');
+
+    // Espera 3 segundos
+    setTimeout(() => {
+        splash.classList.add('fade-out');
+    }, 3000);
+});
 init();
